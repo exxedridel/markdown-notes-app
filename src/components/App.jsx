5e71dcc717fed5506d64cf91e7fcd242358d260a
hevedrios <exxed.ridel@gmail.com>
2022-07-20 16:13:24 -0500

    Definitive React (RTFS) with SASS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "../styles/global.scss";
 
 const App = () => {
   return (
     <div>
-      This is gonna be the Markdown notes SPA
+      <h1>This is gonna be the Markdown notes SPA</h1>
     </div>
-  )
+  );
 };
 
 export default App;
